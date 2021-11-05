de9f377bc8175ed703cb797b93791795d865b43f
ThiagoMalveira <thiago.a.malveira@gmail.com>
2021-11-05 13:00:00 -0300

    adicionando modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from './components/Header';
 import { Middle } from './components/CursoHas';
 import { News } from './components/News'
 import { Subscribe } from './components/Subscribe'
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 import './styles/global.scss'
 
--- src/components/Subscribe/index.tsx
+++ src/components/Subscribe/index.tsx
@@ -1,16 +1,24 @@
-import {FormEvent} from 'react';
 import SP from '../../assets/imgs/unitSP.png'
 import RE from '../../assets/imgs/unitRE.png'
 import DF from '../../assets/imgs/unitDF.png'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 import styles from './styles.module.scss';
+import { useState } from 'react'
 
-async function handleSubscribeCurse(event: FormEvent){
-  console.log("Teste");
-}
+
 
 export function Subscribe (){
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
+    <>
     <div id="Subscribe">
       <div className={styles.titleContainer}>
         <h1>inscreva-se em:</h1>
@@ -18,19 +26,53 @@ export function Subscribe (){
       <div className={styles.locationContainer}>
         <div className={styles.locationContent}>
             <img src={SP} alt="São Paulo" />
-            <button className={styles.buttonSubscribe} onClick={handleSubscribeCurse}>inscreva-se</button>
+            <button className={styles.buttonSubscribe} onClick={handleShow}>inscreva-se</button>
         </div>
         <div className={styles.locationContent}>
           <div className={styles.new}><p>⠀novo</p></div>
           <img src={RE} alt="Recife" />
-          <button className={styles.buttonSubscribe} onClick={handleSubscribeCurse}>inscreva-se</button>
+          <button className={styles.buttonSubscribe} onClick={handleShow}>inscreva-se</button>
         </div>
         <div className={styles.locationContent}>
           <div className={styles.new}><p>⠀novo</p></div>
           <img src={DF} alt="Distrito federal" />
-          <button className={styles.buttonSubscribe} onClick={handleSubscribeCurse}>inscreva-se</button>
+          <button className={styles.buttonSubscribe} onClick={handleShow}>inscreva-se</button>
         </div>
       </div>
     </div>
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Formulário de inscrição</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type="text" placeholder="Nome" required/> 
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Email" required/> 
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Telefone</Form.Label>
+                  <Form.Control type="text" placeholder="Telefone" required/> 
+                </Form.Group>
+
+                <Form.Group className="mt-3">
+                  <ButtonGroup>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Fechar
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Enviar
+                  </Button>
+                  </ButtonGroup>
+                </Form.Group>
+                
+              </Form>
+            </Modal.Body>
+        </Modal>
+      </>
   )
 }
